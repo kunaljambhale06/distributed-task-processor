8aa4b77d7964a928fb95b0d7257ec832133f589a
Kunal Jambhale <jambhalekunal06@gmail.com>
2026-03-13 14:00:53 +0000

    Added job stats api

--- src/routes/jobRoute.js
+++ src/routes/jobRoute.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createJob, getJobs } from "../controllers/jobController.js";
+import { createJob, getJobs, getJobStats } from "../controllers/jobController.js";
 
 const router = Router();
 
 router.post("/", createJob);
-router.get("/", getJobs);   
+router.get("/", getJobs); 
+router.get("/stats", getJobStats);  
 
 export default router;
