3ec381b5564d7025d5a789b9f3ba643997c0caa8
Kunal Jambhale <jambhalekunal06@gmail.com>
2026-03-20 14:38:29 +0000

    Added rate limiter for routes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import connectDB from "./src/config/db.js";
 import jobRoutes from "./src/routes/jobRoute.js";
 import { connectQueue } from "./src/config/rabbitmq.js";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
 
 
 dotenv.config();
@@ -12,14 +13,20 @@ const startServer = async () => {
 
     await connectDB();
     await connectQueue();
+    const limiter = rateLimit({
+        windowMs: 1000,
+        max: 5,
+    });
 
     const app = express();
     app.use(express.json());
     app.use(cors());
 
+    app.use("/jobs", limiter);
     app.use("/jobs", jobRoutes);
     app.use("/uploads", express.static("uploads"));
     app.use("/processed", express.static("processed"));
+    
 
     app.listen(5000, () =>
         console.log(" Server running on port 5000")
